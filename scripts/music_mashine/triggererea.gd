9c70c60d458179a06d9b1b2c712b7053151379d6
DanderOvskii <89800@glr.nl>
2025-11-12 11:01:55 +0100

    small adjustments

--- scripts/music_mashine/triggererea.gd
+++ scripts/music_mashine/triggererea.gd
@@ -6,7 +6,7 @@ signal player_entered
 
 func _ready():
 	print("TriggerArea ready")
-	body_entered.connect(self._on_body_entered)
+	body_entered.connect(_on_body_entered)
 
 
 func _on_body_entered(body:Node3D):
