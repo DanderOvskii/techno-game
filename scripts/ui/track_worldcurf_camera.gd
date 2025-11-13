b8a6a51636a97aa3cd793bf42c593741b2de4561
DanderOvskii <89800@glr.nl>
2025-11-13 18:20:11 +0100

    world curf shader and a masic shader material

---
+++ scripts/ui/track_worldcurf_camera.gd
@@ -0,0 +1,25 @@
+extends Camera3D  # or Camera3D if you put it on the camera
+
+
+@onready var materials := []  # we'll fill this later
+
+func _ready():
+	_find_curved_materials(get_tree().get_root())
+
+func _process(_delta):
+	var cam_pos = global_position
+	for mat in materials:
+		mat.set_shader_parameter("camera_position", cam_pos)
+
+func _find_curved_materials(node):
+	if node is MeshInstance3D:
+		var mesh = node.mesh
+		if !mesh:
+			return
+		for i in range(mesh.get_surface_count()):
+			var mat = node.get_active_material(i)
+			if mat is ShaderMaterial:
+				# Optionally: check for a specific shader name/path
+				materials.append(mat)
+	for child in node.get_children():
+		_find_curved_materials(child)
