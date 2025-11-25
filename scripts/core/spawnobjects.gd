223c47a40c3290e493d91433289fb0677d97d593
DanderOvskii <89800@glr.nl>
2025-11-25 23:00:02 +0100

    random spawn objects

--- scripts/core/spawnobjects.gd
+++ scripts/core/spawnobjects.gd
@@ -4,7 +4,7 @@ extends Node
 @export var spawnpoints : Array[Node3D] = []
 @export var spawnobjects : Array[PackedScene] = []
 @export var spawnchange: Array[float] = []
-@export var editor_button_generate_dungeon : bool = false :
+@export var spawn_object_editor : bool = false :
     set(value):
         spawn_object_at_random_point()
 
@@ -30,14 +30,12 @@ func pick_spawn_object() -> PackedScene:
     return spawnobjects[0]
 
 func spawn_object_at_random_point():
-    
-    # Pick a random spawn point
+    for child in self.get_children():
+        child.queue_free()
     var spawnpoint = spawnpoints[randi() % spawnpoints.size()]
-    
-    # Pick an object based on probability
     var object_to_spawn = pick_spawn_object()
-    
-    # Instance and add it to the scene
     var instance = object_to_spawn.instantiate()
     instance.position = spawnpoint.position
+    instance.transform.origin = spawnpoint.transform.origin
+
     add_child(instance)
