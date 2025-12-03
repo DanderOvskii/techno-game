aa863d520b3972c53af5c736bb17c19dc4f8bba9
DanderOvskii <89800@glr.nl>
2025-12-03 16:06:36 +0100

    random shit

--- scripts/core/spawnobjects.gd
+++ scripts/core/spawnobjects.gd
@@ -4,38 +4,56 @@ extends Node
 @export var spawnpoints : Array[Node3D] = []
 @export var spawnobjects : Array[PackedScene] = []
 @export var spawnchange: Array[float] = []
-@export var spawn_object_editor : bool = false :
-    set(value):
-        spawn_object_at_random_point()
+@export var spawn_object_editor := false:
+	set(value):
+		spawn_object_editor = value
+		if value:
+			spawn_object_at_random_point()
+
+var spawned_instance: Node3D
 
 func _ready():
-    spawn_object_at_random_point()
+	spawn_object_at_random_point()
   
 
 
 
 func pick_spawn_object() -> PackedScene:
-    var total_prob = 0.0
-    for prob in spawnchange:
-        total_prob += prob
-    
-    var rand = randf() * total_prob
-    var running_sum = 0.0
-    
-    for i in range(spawnobjects.size()):
-        running_sum += spawnchange[i]
-        if rand <= running_sum:
-            return spawnobjects[i]
-    
-    return spawnobjects[0]
+	if spawnobjects.is_empty() or spawnchange.is_empty():
+		push_warning("Spawn objects or spawnchange is empty!")
+		return null
+
+	if spawnobjects.size() != spawnchange.size():
+		push_warning("spawnobjects and spawnchange have different sizes!")
+		return spawnobjects[0]
+
+	var total_prob = 0.0
+	for prob in spawnchange:
+		total_prob += prob
+	
+	var rand = randf() * total_prob
+	var running_sum = 0.0
+	
+	for i in range(spawnobjects.size()):
+		running_sum += spawnchange[i]
+		if rand <= running_sum:
+			return spawnobjects[i]
+	
+	return spawnobjects[0]
 
 func spawn_object_at_random_point():
-    for child in self.get_children():
-        child.queue_free()
-    var spawnpoint = spawnpoints[randi() % spawnpoints.size()]
-    var object_to_spawn = pick_spawn_object()
-    var instance = object_to_spawn.instantiate()
-    instance.position = spawnpoint.position
-    instance.transform.origin = spawnpoint.transform.origin
+	if spawnpoints.is_empty():
+		push_warning("No spawnpoints assigned!")
+		return
 
-    add_child(instance)
+	if spawned_instance and is_instance_valid(spawned_instance):
+		spawned_instance.queue_free()
+	for child in self.get_children(): child.queue_free()	
+	var spawnpoint = spawnpoints[randi() % spawnpoints.size()]
+	var object_to_spawn = pick_spawn_object()
+	spawned_instance = object_to_spawn.instantiate()
+	spawned_instance.position = spawnpoint.position
+	spawned_instance.transform.origin = spawnpoint.transform.origin
+
+	add_child(spawned_instance)
+
